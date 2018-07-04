54b8f5d80fa18b96cda09930183619bc8bb13147
liweiwei <1651399352@qq.com>
2018-07-04 20:54:27 +0800

    图片上传与显示

--- src/main/java/com/example/cloudstore/controller/UserInfoController.java
+++ src/main/java/com/example/cloudstore/controller/UserInfoController.java
@@ -3,12 +3,25 @@ package com.example.cloudstore.controller;
 import com.example.cloudstore.domain.Result;
 import com.example.cloudstore.domain.entity.SysUser;
 import com.example.cloudstore.domain.entity.UserInfo;
+import com.example.cloudstore.service.IconService;
 import com.example.cloudstore.service.SysUserService;
 import com.example.cloudstore.service.UserInfoService;
 import com.example.cloudstore.utils.ResultUtil;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FSDataInputStream;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.URI;
 
 @RestController
 public class UserInfoController {
@@ -22,6 +35,10 @@ public class UserInfoController {
     private PasswordEncoder passwordEncoder;
     @Autowired
     private GlobalFunction globalFunction;
+    @Autowired
+    private IconService iconService;
+
+    private String HADOOP_URL = "hdfs://192.168.59.145:9000";
 
     /**
      * 忘记密码
@@ -65,7 +82,7 @@ public class UserInfoController {
     }
 
     /**
-     * 添加（修改）个人信息
+     * 修改个人信息
      */
     @PostMapping("/user/info")
     public Result userInfo( UserInfo userInfo){
@@ -86,11 +103,57 @@ public class UserInfoController {
     }
 
     /**
-     * 上传头像
+     * 上传（修改）头像
+     * @param src 头像的路径如“C:\Users\liweiwei\Pictures\psb.png”
+     * @return
+     * @throws IOException
      */
-
+    @PostMapping("/user/upload/icon")
+    public Result uploadImageFile(String src) throws IOException {
+        String username = globalFunction.getUsername();
+        String dst = "/userIcon/" + username + ".jpg";
+        String IconPath = iconService.uploadImageFile(src,dst);
+        //插入数据库
+        UserInfo userInfo = userInfoService.findByUsername(username);
+        userInfo.setIcon(IconPath);
+        userInfoService.insert(userInfo);
+        return ResultUtil.success(IconPath);
+    }
 
     /**
      * 获取头像
      */
+    @GetMapping("/usr/icon")
+    public void readFileByAPI(HttpServletResponse response) throws IOException {
+//        String iconPath = "hdfs://192.168.59.145:9000/userIcon/lww.jpg";
+        String username = globalFunction.getUsername();
+        UserInfo userInfo = userInfoService.findByUsername(username);
+        String iconPath = userInfo.getIcon();
+
+        //读取配置文件
+        Configuration conf = new Configuration();
+        //获取文件系统
+        FileSystem fs = FileSystem.get(URI.create(HADOOP_URL),conf);
+
+        //获取路径
+        Path p = new Path(iconPath);
+        //通过文件系统打开路径获取HDFS文件输入流
+        FSDataInputStream fis =  fs.open(p);
+        //创建缓冲区
+        byte[] buf = new byte[1024*1024];
+        int len = -1;
+        //当当读取的长度不等于-1的时候开始写入
+        //写入需要字节输出流
+        OutputStream baos = response.getOutputStream();
+        while ((len = fis.read(buf)) != -1){
+            baos.write(buf,0,len);
+        }
+        //写入完毕，关闭输入流
+        fis.close();
+        //关闭输出流
+        baos.close();
+    }
+
+
+
 }
---
+++ src/main/java/com/example/cloudstore/service/IconService.java
@@ -0,0 +1,32 @@
+package com.example.cloudstore.service;
+
+import java.io.IOException;
+
+public interface IconService {
+
+    /**
+     * 创建文件夹
+     * 返回文件夹在文件系统的全路径
+     * @param path  example:/usr/dir1; /usr
+     * @throws IOException
+     */
+    boolean createDir(String path) throws IOException;
+
+    /**
+     * 上传头像
+     * 返回头像在文件系统的全路径
+     * @param src 上传的头像的源路径
+     * @param dst  上传到文件系统上的目标路径
+     * @throws IOException
+     */
+    String uploadImageFile(String src, String dst) throws IOException;
+
+
+    /**
+     * 删除文件或目录
+     * @param filePath
+     * @return
+     * @throws IOException
+     */
+    boolean deleteFile(String filePath) throws IOException;
+}
