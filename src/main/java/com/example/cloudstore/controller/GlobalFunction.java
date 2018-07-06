0e3552aa91e8b48a7a5c3149a55f2810524e95e6
shihaowei <1581258627@qq.com>
2018-07-06 10:20:12 +0800

    获取当前登录的用户名和文件系统上的目录大小

--- src/main/java/com/example/cloudstore/controller/GlobalFunction.java
+++ src/main/java/com/example/cloudstore/controller/GlobalFunction.java
@@ -1,17 +1,26 @@
 package com.example.cloudstore.controller;
 
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FileStatus;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
 import org.springframework.security.authentication.AnonymousAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+
 @Component
 public class GlobalFunction {
     /**
      * 获取当前登录用户的用户名
+     *
      * @return
      */
-    public String getUsername(){
+    public String getUsername() {
 //        UserDetails userDetails = (UserDetails) SecurityContextHolder.getContext().getAuthentication() .getPrincipal();
 //        String username = userDetails.getUsername();
 //        return username;
@@ -21,6 +30,30 @@ public class GlobalFunction {
             return currentUserName;
         }
         return null;
+    }
+
+    public String getDirectorySize(String path) throws URISyntaxException, IOException {
+        FileSystem hdfs = null;
+        Configuration config = new Configuration();
+        // 程序配置
+        config.set("fs.default.name", "hdfs://maste:9000");
+        //config.set("hadoop.job.ugi", "feng,111111");
+        //config.set("hadoop.tmp.dir", "/tmp/hadoop-fengClient");
+        //config.set("dfs.replication", "1");
+        //config.set("mapred.job.tracker", "master:9001");
+        hdfs = FileSystem.get(new URI("hdfs://maste:9000"), config);
+//        Path path = new Path("/");
+        Path newPath = new Path(path);
+        FileStatus fileStatus = hdfs.getFileStatus(newPath);
+//        System.out.println("dir3的文件夹大小是：" + hdfs.getContentSummary(new Path(path)).getLength());
+
+        if (fileStatus.isDirectory()){
+            return String.valueOf(hdfs.getContentSummary(new Path(path)).getLength());
+        }
+        if (fileStatus.isFile()){
+            return "Error. It's not a directory";
+        }
+        return "Error.Input is error";
     }
 
 
