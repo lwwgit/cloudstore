16a5944acca048bf4178ae94602a80d0c560b96b
shihaowei <1581258627@qq.com>
2018-07-08 16:27:20 +0800

    文件查找

--- src/main/java/com/example/cloudstore/service/impl/SearchServiceImpl.java
+++ src/main/java/com/example/cloudstore/service/impl/SearchServiceImpl.java
@@ -6,6 +6,7 @@ import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.FileStatus;
 import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
+import org.powermock.core.ListMap;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
@@ -40,15 +41,15 @@ public class SearchServiceImpl implements SearchService {
         config.set("fs.default.name", HdfsPath);
         hdfs = FileSystem.get(new URI(HdfsPath), config);
         Path path = new Path("/" + name); //确定搜索文件夹  new Path("/" + dir)，dir为文件夹的绝对路径
-        List<Map<String, Object>> ListMap = new ArrayList<>();
+        List<Map<String, Object>> listMap = new ArrayList<>();
 
         Pattern pattern = Pattern.compile(SearchWord);//处理关键字
-        ReturnSearch(hdfs, path, ListMap, pattern);//遍历目录，列出所有文件的ListMap
+        ReturnSearch(hdfs, path, listMap, pattern);//遍历目录，列出所有文件的ListMap
 
-        return ListMap;
+        return listMap;
     }
 
-    public void ReturnSearch(FileSystem hdfs, Path path, List<Map<String, Object>> ListMap, Pattern pattern) throws IOException {
+    public void ReturnSearch(FileSystem hdfs, Path path, List<Map<String, Object>> listMap, Pattern pattern) throws IOException {
 
         FileStatus[] files = hdfs.listStatus(path);//获取目录下所有文件（包括目录）的信息
 
@@ -59,7 +60,7 @@ public class SearchServiceImpl implements SearchService {
         for (int i = 0; i < files.length; i++) {
             if (files[i].isDirectory()) {
                 //递归调用
-                ReturnSearch(hdfs, files[i].getPath(), ListMap, pattern);
+                ReturnSearch(hdfs, files[i].getPath(), listMap, pattern);
             }
 
             if (files[i].isFile()) {
@@ -67,16 +68,16 @@ public class SearchServiceImpl implements SearchService {
                 Matcher matcher = pattern.matcher(files[i].getPath().getName());
                 while (matcher.find()) {
                     Map<String, Object> list = new HashMap<>();
-                    list.put("Name", files[i].getPath().getName());
+                    list.put("name", files[i].getPath().getName());
 
                     String truePath = files[i].getPath().toString().substring(files[i].getPath().toString().lastIndexOf("9000") + 4);
-                    list.put("Path", truePath);
+                    list.put("path", truePath);
 //                    list.put("Path", files[i].getPath().toString());
-                    list.put("ModificationTime", formatter.format(files[i].getModificationTime()));
+                    list.put("modificationTime", formatter.format(files[i].getModificationTime()));
                     list.put("length", files[i].getLen());
                     list.put("type", suffix);
 
-                    ListMap.add(list);
+                    listMap.add(list);
                 }
 
             }
